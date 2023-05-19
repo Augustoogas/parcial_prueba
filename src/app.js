7b67f7ca3a9240da6414d94436e27daf2859971a
Augusto ogas <augustoogas7@gmail.com>
2023-05-19 00:37:20 -0300

    Creacion de app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("Hola mundo");
+const express=require('express');
+const PORT=process.env.PORT||3001;
+const app=express();
+const ruta=require('./route/route.js');
+
+
+app.use(express.json());
+app.use("/",ruta);
+
+app.listen(PORT,()=>{console.log(`la app esta escuchando en el puerto ${PORT}`)});
+
